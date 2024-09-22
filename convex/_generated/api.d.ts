53c8f639bf39bc3b55d5a7fa8a3b2cf7e46dbc8c
Brandon Mullins <webdevmullins@gmail.com>
2024-09-22 17:44:49 -0500

    feat: convex adds user to db on sign up or when they update their account

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,7 +15,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as test from "../test.js";
+import type * as http from "../http.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,7 +27,8 @@ import type * as test from "../test.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  test: typeof test;
+  http: typeof http;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
